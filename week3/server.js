022714c6bb090269fe0aa322e39ba875e5d43855
Yeyul_Choi <yeyulchoi@outlook.com>
2023-05-22 18:55:57 -0400

    added typescript support

--- week3/server.js
+++ week3/server.js
@@ -1,40 +1,30 @@
-//reuire built-in modules   Common js
-
-const http = require('http');
-const fs = require('fs');
-const mime = require('mime-types');
-
-let lookup = mime.lookup; //alias for mime.lookup
-const port=3000;
-
-//when we create a server instance, it is IMMUTABLE = cannot be changed until the server is restarted
-const server = http.createServer(function(req,res)
-{
-    let path = req.url; //alias for req.rul
-
-    if(path =="/" || path=="/home"){
-        path= "/index.html";
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const fs_1 = __importDefault(require("fs"));
+const mime_types_1 = __importDefault(require("mime-types"));
+let lookup = mime_types_1.default.lookup;
+const port = 3000;
+const server = http_1.default.createServer(function (req, res) {
+    let path = req.url;
+    if (path == "/" || path == "/home") {
+        path = "/index.html";
     }
-
     let mime_type = lookup(path.substring(1));
-    // console.log(`mime type is ${mime_type}`);
-    
-    fs.readFile(__dirname+path,function(err,data)
-    {
-        if(err){
-            res.writeHead(404); //status  file not found
-            return res.end("ERROR:404 - File Not Found"); //output the error to the page
-            
+    fs_1.default.readFile(__dirname + path, function (err, data) {
+        if (err) {
+            res.writeHead(404);
+            return res.end("ERROR:404 - File Not Found");
         }
-        //no error
-        res.setHeader("X-Content-Type-Options", "nosniff") //security guard
-        res.writeHead(200, {'Content-Type':mime_type}); //status all OKAY
-        return res.end(data); //output the file that was read to the page
-        
+        res.setHeader("X-Content-Type-Options", "nosniff");
+        res.writeHead(200, { 'Content-Type': mime_type });
+        return res.end(data);
     });
 });
-
-server.listen(port, function()
-{
+server.listen(port, function () {
     console.log(`Server is running at port ${port}`);
 });
+//# sourceMappingURL=server.js.map
