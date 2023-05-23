cc01c08502504b96d8d37baee049e3d650c2b56a
Yeyul_Choi <yeyulchoi@outlook.com>
2023-05-22 20:20:24 -0400

    convert server to Express

--- week3/server.js
+++ week3/server.js
@@ -3,28 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = __importDefault(require("http"));
-const fs_1 = __importDefault(require("fs"));
-const mime_types_1 = __importDefault(require("mime-types"));
-let lookup = mime_types_1.default.lookup;
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const port = 3000;
-const server = http_1.default.createServer(function (req, res) {
-    let path = req.url;
-    if (path == "/" || path == "/home") {
-        path = "/index.html";
-    }
-    let mime_type = lookup(path.substring(1));
-    fs_1.default.readFile(__dirname + path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            return res.end("ERROR:404 - File Not Found");
-        }
-        res.setHeader("X-Content-Type-Options", "nosniff");
-        res.writeHead(200, { 'Content-Type': mime_type });
-        return res.end(data);
-    });
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
-server.listen(port, function () {
-    console.log(`Server is running at port ${port}`);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
 //# sourceMappingURL=server.js.map
